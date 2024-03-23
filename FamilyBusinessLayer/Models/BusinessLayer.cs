a97f3a7ab78cd69ebd56c0c509bf41000e498c2c
Jesmi Raithore <jesmiraithore@gmail.com>
2024-03-23 16:23:12 +0000

    Added edit functionality in the controller and respository of business layer

--- FamilyBusinessLayer/Models/BusinessLayer.cs
+++ FamilyBusinessLayer/Models/BusinessLayer.cs
@@ -85,5 +85,51 @@ namespace FamilyBusinessLayer.Models
 
             }
         }
+
+
+        public void SaveFamilymember(FamilyMemberDetails familyMemberDetails)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DatabaseContext"].ConnectionString;
+
+            using (SqlConnection connection= new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand("spSaveFamilyMember", connection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter sqlParameterFamilyMemberId = new SqlParameter();
+                sqlParameterFamilyMemberId.ParameterName = "@FamilyMemberId";
+                sqlParameterFamilyMemberId.Value = familyMemberDetails.FamilyMemberId;
+                sqlCommand.Parameters.Add(sqlParameterFamilyMemberId);
+
+                SqlParameter sqlParameterName = new SqlParameter();
+                sqlParameterName.ParameterName = "@Name";
+                sqlParameterName.Value = familyMemberDetails.Name;
+                sqlCommand.Parameters.Add(sqlParameterName);
+
+                SqlParameter sqlParameterGender = new SqlParameter();
+                sqlParameterGender.ParameterName = "@Gender";
+                sqlParameterGender.Value = familyMemberDetails.Gender;
+                sqlCommand.Parameters.Add(sqlParameterGender);
+
+                SqlParameter sqlParameterCity = new SqlParameter();
+                sqlParameterCity.ParameterName = "@City";
+                sqlParameterCity.Value = familyMemberDetails.City;
+                sqlCommand.Parameters.Add(sqlParameterCity);
+
+                SqlParameter sqlParameterJob = new SqlParameter();
+                sqlParameterJob.ParameterName = "@Job";
+                sqlParameterJob.Value = familyMemberDetails.Job;
+                sqlCommand.Parameters.Add(sqlParameterJob);
+
+                SqlParameter sqlParameterFamilyMemberTypeId = new SqlParameter();
+                sqlParameterFamilyMemberTypeId.ParameterName = "@FamilyMemberTypeId";
+                sqlParameterFamilyMemberTypeId.Value = familyMemberDetails.FamilyMemberTypeId;
+                sqlCommand.Parameters.Add(sqlParameterFamilyMemberTypeId);
+
+                connection.Open();
+                sqlCommand.ExecuteNonQuery();
+                
+            }
+        }
     }
 }
